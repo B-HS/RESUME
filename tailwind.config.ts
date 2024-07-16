76173779c5407b96f372b876b9a3e2b94cb670cb
Hyunseok Byun <gumyoincirno@gmail.com>
2024-07-16 23:27:03 +0900

    Update font and meta data, remove unused dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,13 @@
 import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config: Config = {
-    content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+    content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ['var(--font-mplus-rounded-1c)', ...fontFamily.sans],
+            },
             backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
