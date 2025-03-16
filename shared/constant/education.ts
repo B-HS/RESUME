b206481facbf9e4dfd293a378dd8950254a6f2fc
Hyunseok Byun <gumyoincirno@gmail.com>
2025-03-16 14:45:19 +0900

    Update histories

--- shared/constant/education.ts
+++ shared/constant/education.ts
@@ -1,4 +1,5 @@
 export const educationAndEtc = [
+    { duration: '25.02 ~', description: '고려 사이버대학교 컴퓨터공학과' },
     { duration: '14.03 ~ 19.02', description: '마산대학교 관광일본어' },
     { duration: '18.09 ~ 19.01', description: '夢乃井 인턴쉽' },
     { duration: '19.05', description: '6th 동아시아 농업유산 학회 현장견학 KR - JP 통역' },
